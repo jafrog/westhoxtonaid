8a631a1a4d995972dbdba900cadaa62f3ad7f855
Irina Bednova <irina.bednova@gmail.com>
2020-03-30 20:26:29 +0100

    Set up Netlify CMS - 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,14 +40,14 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-  //`gatsby-plugin-netlify-cms`,
-  //   {
-  //   resolve: `gatsby-source-filesystem`,
-  //   options: {
-  //     name: `markdown-pages`,
-  //     path: `${__dirname}/content`,
-  //   },
-  // },
+  `gatsby-plugin-netlify-cms`,
+    {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/content`,
+    },
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
